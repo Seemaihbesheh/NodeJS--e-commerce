102406ec93284b995c7895d6d1ea2f274d46eaa5
nourKurdii <67168695+nourKurdii@users.noreply.github.com>
2024-01-23 23:10:58 +0200

    Update productRoutes.ts

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -5,16 +5,16 @@ import { sessionMiddleware } from '../middlewares/sessionMiddleware'
 
 
 router.get('/trendy', getTrendyProducts)
-router.get("/category/:category" ,getProductsByCategory)
-router.get("/brand/:brand" ,getProductsByBrand)
-router.get("/newArrival" ,getNewArrivalProducts)
-router.get("/limited", getLimitedProducts)
-router.get("/discount/:discount", getProductsByDiscoutOrMore)
+router.get('/category/:category', getProductsByCategory)
+router.get('/brand/:brand', getProductsByBrand)
+router.get('/new-arrival', getNewArrivalProducts)
+router.get('/limited', getLimitedProducts)
+router.get('/discount/:discount', getProductsByDiscoutOrMore)
 
 
-router.get('/handpicked',handPicked);
-router.get('/:productID' , getSpecificProduct);
-router.post('/rate/:productID',sessionMiddleware,rateProduct);
+router.get('/handpicked', handPicked)
+router.get('/:productID', getSpecificProduct)
+router.post('/rate/:productID', sessionMiddleware,rateProduct)
 router.get('/ratings-and-reviews/:productID', getRateAndReview)
 
 
