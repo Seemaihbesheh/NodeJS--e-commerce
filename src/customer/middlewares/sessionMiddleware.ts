de1f2ca91cced86a6b22e29ee4080f4086633021
RaghadDabbous2 <raghad.dabbous22@gmail.com>
2024-01-28 21:02:09 +0200

    refactor findAllProducts

--- src/customer/middlewares/sessionMiddleware.ts
+++ src/customer/middlewares/sessionMiddleware.ts
@@ -2,29 +2,62 @@ import { sessionModel, userModel } from '../../models/modelsRelations'
 import { NextFunction, Request, Response } from 'express'
 interface CustomRequest extends Request {
   session?: any,
-  user?: any
+  user? :any
 }
+//this middleware for authorized requests
 const sessionMiddleware = async (req: CustomRequest, res: Response, next: NextFunction) => {
   try {
     const { headers: headersData } = req
+    console.log(req.headers);
     if (!headersData.authorization) {
       return res.status(400).json({ error: 'Session ID not provided in headers' })
     }
-    const foundSession = await sessionModel.findOne({ where: { sessionID: headersData.authorization } })
+
+    isAuth(req , res , next)
+
+  } catch (error) {
+    next(error)
+  }
+}
+//this middleware for authorized and unauthorized requests
+const optionalAutorization = async (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    const { headers: headersData } = req
+    console.log(req.headers);
+    if (!headersData.authorization) {
+      req.user = null;
+      next();
+      return;
+    }
+
+    isAuth(req , res , next)
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function isAuth(req , res  , next) {
+  try{
+
+    const foundSession = await sessionModel.findOne({ where: { sessionID: req.headers.authorization } })
+
     if (foundSession) {
-      const foundUser = await userModel.findOne({ where: { userID: foundSession.userID } })
+      const user = await userModel.findByPk(foundSession.dataValues.userID);
+      req.user = user;
       req.session = foundSession
-      req.user = foundUser
       next()
     } else {
-      return res.status(400).json({ error: 'Session not found or timed out. Please log in again.' })
+      return res.status(400).json({error: 'Session not found or timed out. Please log in again.'})
     }
-  } catch (error) {
+
+  }catch (error) {
     next(error)
   }
 }
 
 export {
   sessionMiddleware,
+  optionalAutorization,
   CustomRequest,
 } 
