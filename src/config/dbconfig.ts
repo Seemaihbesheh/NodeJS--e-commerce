f716739d721ca0316be4137ffd48ea75efbc9427
nourKurdii <67168695+nourKurdii@users.noreply.github.com>
2024-01-29 20:47:41 +0200

    Update dbconfig.ts

--- src/config/dbconfig.ts
+++ src/config/dbconfig.ts
@@ -3,7 +3,7 @@ import { Dialect } from "sequelize"
 const dbConfig = {
     HOST: 'localhost',
     USER: 'root',
-    PASSWORD: 'seema123.I',
+    PASSWORD: '12345678',
     DB: 'coraldatabase',
     dialect: 'mysql' as Dialect,
     pool: {
