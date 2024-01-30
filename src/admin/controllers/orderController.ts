36d077ddd40b95c1fc357adf954d07c7a4560ac8
SeemaIhbesheh <73760401+Seemaihbesheh@users.noreply.github.com>
2024-01-30 23:01:24 +0200

    Update orderController.ts

--- src/admin/controllers/orderController.ts
+++ src/admin/controllers/orderController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import * as orderSevices from "../../services/orderServices";
-
+import  * as validates from '../../validators/validateSchema'
 export const updateOrderStatus = async function (
   req: Request,
   res: Response
@@ -8,7 +8,15 @@ export const updateOrderStatus = async function (
   const orderID = Number(req.params.orderID)
   const newStatus = req.body.status
 
+
+
   try {
+
+    const validationResult = validates.orderValidationSchema.validate({ orderID:orderID, state:newState })
+
+    if (validationResult.error) {
+        return res.status(400).json("Invalid Input");
+    }
     const updatedOrder = await orderSevices.updateOrderStatus(
       orderID,
       newStatus
